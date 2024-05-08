ac94152f116b08056792e0f00eb64edf7e2017b2
hamtol2 <ronniej@naver.com>
2024-05-08 13:30:26 +0900

    간단한 이동 로직 작성
    - CustomCube 스크립트에 Transform을 활용한 이동 기능 추가함

--- Assets/Scripts/CustomCube.cs
+++ Assets/Scripts/CustomCube.cs
@@ -8,11 +8,31 @@ using UnityEngine;
 
 public class CustomCube : MonoBehaviour
 {
+    // 필드. 변수(변하는 수-Variable).
+    // public -> 외부 공개 + 인스펙터에 노출.
+    // Attribute.
+    [SerializeField]
+    private Transform refTransform;
+
+    // X축의 이동 속도.
+    [SerializeField]
+    private float xSpeed = 0.01f;
+
     // Start is called before the first frame update
+    // 이벤트 함수(메소드) -> 엔진이 알아서 실행해준다.
     void Start()
     {
         // Console 창에 문자를 출력해주는 메소드.
         Debug.Log("Start");
+
+        // 컴포넌트 검색하기.
+        // 외우기.
+        //refTransform = GetComponent<Transform>();
+        refTransform = transform;
+
+        // 이동.
+        // 디버깅(1. 문제찾기 2. 문제해결).
+        refTransform.Translate(1f, 0f, 0f);
     }
 
     // Update is called once per frame
@@ -20,5 +40,8 @@ public class CustomCube : MonoBehaviour
     {
         // Console 창에 문자를 출력해주는 메소드.
         Debug.Log("Update");
+
+        // 프레임 마다 조금씩 이동.
+        refTransform.Translate(xSpeed, 0f, 0f);
     }
 }
