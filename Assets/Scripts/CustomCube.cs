00eafc1aa36e3f0b26547805070bb6998a0bf350
hamtol2 <ronniej@naver.com>
2024-05-08 11:31:07 +0900

    유니티 2일차 수업 첫시간
    - 머티리얼 변경해봄

---
+++ Assets/Scripts/CustomCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Home / End.
+// Shift + Home  / Shift + End
+// Ctrl + 방향키(좌우).
+
+public class CustomCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Console 창에 문자를 출력해주는 메소드.
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Console 창에 문자를 출력해주는 메소드.
+        Debug.Log("Update");
+    }
+}
